5dbfb052310e40f0979e352d620c39b999570a57
Eric Bailey <eric.w.bailey@gmail.com>
2016-12-24 18:39:29 -0500

    Added for loop that turns mailto links into buttons

---
+++ source/main.js
@@ -0,0 +1,11 @@
+var mailtoLinks = document.querySelectorAll( '[data-message-subject]' )
+
+// Turns mailto links into buttons for use with a11y-dialog
+for ( var i = 0; i < mailtoLinks.length; i++ ) {
+  var contactButton = mailtoLinks[i]; // Turns querySelectorAll results into something workable
+  var contactButtonClasses = contactButton.getAttribute( 'class' );
+  var contactButtonSubject = contactButton.getAttribute( 'data-message-subject' );
+  var contactButtonContent = contactButton.innerHTML;
+
+  contactButton.outerHTML = '<button class="' + contactButtonClasses + '" data-message-subject="' + contactButtonSubject + '">' + contactButtonContent + '</button>'
+}
