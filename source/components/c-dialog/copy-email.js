396e3bbb6597039becc0b10d29f48c3ac1ddff1b
Eric Bailey <eric.w.bailey@gmail.com>
2017-01-20 20:20:34 -0500

    Disabled JS partials and moved into main.js

--- source/components/c-dialog/copy-email.js
+++ source/components/c-dialog/copy-email.js
@@ -9,36 +9,36 @@
  * @example resetCopyButton()
  * @public
  */
-function resetCopyButton() {
-  copyButton.classList.remove( 'is-successful' );
-  copyButton.innerHTML = '<svg height="14" width="14" aria-hidden="true" viewBox="0 0 100 100" class="icon c-button__icon"><use xlink:href="#icon-clipboard"></use></svg>Copy<span class="content-hide-visually"> email address</span>';
-}
-
-
-var clipboard = new Clipboard( '.c-button__copy' );
-
-// Adds a success state when the email address is successfully copied
-clipboard.on('success', function( e ) {
-  copyButton.classList.add( 'is-successful' );
-  copyButton.setAttribute( 'aria-live', 'polite' );
-  copyButton.innerHTML = '<svg height="14" width="14" aria-hidden="true" viewBox="0 0 100 100" class="icon c-button__icon"><use xlink:href="#icon-checkmark"></use></svg><span class="content-hide-visually">Email address </span>Copied';
-
-  e.clearSelection();
-
-  setTimeout( resetCopyButton, 2500 );
-});
-
-
-// Displays an error state if the email address does not successfully copy
-clipboard.on('error', function( e ) {
-  var contactField = document.getElementById( 'button-copy' );
-  var contactError = document.createElement( 'p' );
-  var contactFieldParent = contactField.parentNode;
-
-  copyButton.disabled = true;
-
-  contactFieldParent.insertBefore(contactError, contactField.nextSibling); // Generate the error message after the contact field
-  contactError.innerHTML = 'Your browser does not have clipboard support. Try copying the email address manually.';
-  contactError.setAttribute( 'class', 'c-button__error' );
-  contactError.setAttribute( 'aria-live', 'polite' );
-});
+// function resetCopyButton() {
+//   copyButton.classList.remove( 'is-successful' );
+//   copyButton.innerHTML = '<svg height="14" width="14" aria-hidden="true" viewBox="0 0 100 100" class="icon c-button__icon"><use xlink:href="#icon-clipboard"></use></svg>Copy<span class="content-hide-visually"> email address</span>';
+// }
+//
+//
+// var clipboard = new Clipboard( '.c-button__copy' );
+//
+// // Adds a success state when the email address is successfully copied
+// clipboard.on('success', function( e ) {
+//   copyButton.classList.add( 'is-successful' );
+//   copyButton.setAttribute( 'aria-live', 'polite' );
+//   copyButton.innerHTML = '<svg height="14" width="14" aria-hidden="true" viewBox="0 0 100 100" class="icon c-button__icon"><use xlink:href="#icon-checkmark"></use></svg><span class="content-hide-visually">Email address </span>Copied';
+//
+//   e.clearSelection();
+//
+//   setTimeout( resetCopyButton, 2500 );
+// });
+//
+//
+// // Displays an error state if the email address does not successfully copy
+// clipboard.on('error', function( e ) {
+//   var contactField = document.getElementById( 'button-copy' );
+//   var contactError = document.createElement( 'p' );
+//   var contactFieldParent = contactField.parentNode;
+//
+//   copyButton.disabled = true;
+//
+//   contactFieldParent.insertBefore(contactError, contactField.nextSibling); // Generate the error message after the contact field
+//   contactError.innerHTML = 'Your browser does not have clipboard support. Try copying the email address manually.';
+//   contactError.setAttribute( 'class', 'c-button__error' );
+//   contactError.setAttribute( 'aria-live', 'polite' );
+// });
