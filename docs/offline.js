41e80d15ccb216c994fa7c9a1fb531c4a6de1b57
Eric Bailey <eric.w.bailey@gmail.com>
2017-08-04 11:18:33 -0400

    Deployed site on Fri Aug 04 2017 11:18:29 GMT-0400 (EDT)

---
+++ docs/offline.js
@@ -0,0 +1,39 @@
+// Determines if the browser has lost connection
+// https://mxb.at/blog/youre-offline/
+let isOffline = false;
+
+window.addEventListener('load', checkConnectivity);
+
+// Listen for future changes in connection when the page has finished loading
+function checkConnectivity() {
+  updateStatus();
+  window.addEventListener('online', updateStatus);
+  window.addEventListener('offline', updateStatus);
+}
+
+// Check if we're online, set a class on `<html>` if not
+function updateStatus() {
+  if (typeof navigator.onLine !== 'undefined') {
+    isOffline = !navigator.onLine;
+    document.documentElement.classList.toggle('is-offline', isOffline);
+
+    // Toggle `hidden` attribute
+    var filter = document.querySelector('.t-offline');
+    if (!isOffline) {
+      filter.setAttribute('hidden', '');
+    } else {
+      filter.removeAttribute('hidden');
+    }
+  }
+}
+
+
+const links = document.querySelectorAll('a[href]');
+
+Array.from(links).forEach((link) => {
+  caches.match(link.href, { ignoreSearch: true }).then((response) => {
+    if (response) {
+      link.classList.add('is-cached');
+    }
+  });
+});
