c08547c5a41175bb80a525773e39eac287975644
Eric Bailey <eric.w.bailey@gmail.com>
2020-11-30 21:52:37 -0500

    Clean up .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,53 +1,52 @@
 const { DateTime } = require("luxon");
 const fs = require("fs");
-const pluginRss = require("@11ty/eleventy-plugin-rss");
-const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
-const pluginNavigation = require("@11ty/eleventy-navigation");
 const markdownIt = require("markdown-it");
 const markdownItAnchor = require("markdown-it-anchor");
+const pluginNavigation = require("@11ty/eleventy-navigation");
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const slugify = require("slugify");
 
 
 module.exports = function(eleventyConfig) {
+  // Plugins
   eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.addPlugin(pluginSyntaxHighlight);
   eleventyConfig.addPlugin(pluginNavigation);
 
-  eleventyConfig.setDataDeepMerge(true);
 
+  // Copy
+  eleventyConfig.addPassthroughCopy("./source/img");
+  eleventyConfig.addPassthroughCopy("./source/js");
+  eleventyConfig.addPassthroughCopy("./source/static");
+  eleventyConfig.addPassthroughCopy("./source/browserconfig.xml");
+  eleventyConfig.addPassthroughCopy("./source/CNAME");
+  eleventyConfig.addPassthroughCopy("./source/crossdomain.xml");
+  eleventyConfig.addPassthroughCopy("./source/humans.txt");
+  eleventyConfig.addPassthroughCopy("./source/favicon.ico");
+  eleventyConfig.addPassthroughCopy("./source/favicon-16x16.png");
+  eleventyConfig.addPassthroughCopy("./source/favicon-32x32.png");
+  eleventyConfig.addPassthroughCopy("./source/favicon-48x48.png");
+  eleventyConfig.addPassthroughCopy("./source/favicon.svg");
+  eleventyConfig.addPassthroughCopy("./source/manifest.json");
+  eleventyConfig.addPassthroughCopy("./source/robots.txt");
+
+
+  // Aliases
   eleventyConfig.addLayoutAlias("post", "layouts/post.njk");
 
+
+  // Watch targets
+  eleventyConfig.addWatchTarget("./source/sass/");
+
+
+  // Filters
   eleventyConfig.addFilter("readableDate", dateObj => {
     return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("EEEE MMMM d, yyyy");
   });
 
-  // Universal slug filter strips unsafe chars from URLs
-  eleventyConfig.addFilter("slugify", function (str) {
-    return slugify(str, {
-      lower: true,
-      replacement: "-",
-      remove: /[*+~.·,()'"`´%!?¿:@»]/g
-    });
-  });
 
-  // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
-  eleventyConfig.addFilter('htmlDateString', (dateObj) => {
-    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
-  });
-
-  // Get the first `n` elements of a collection.
-  eleventyConfig.addFilter("head", (array, n) => {
-    if( n < 0 ) {
-      return array.slice(n);
-    }
-
-    return array.slice(0, n);
-  });
-
-  eleventyConfig.addFilter("min", (...numbers) => {
-    return Math.min.apply(null, numbers);
-  });
-
+  // Collections
   // Treat writing/ as a collection
   eleventyConfig.addCollection("posts", function(collectionApi) {
     return collectionApi.getFilteredByGlob("writing/*.md");
@@ -82,25 +81,41 @@ module.exports = function(eleventyConfig) {
     return [...tagSet];
   });
 
-  eleventyConfig.addWatchTarget("./source/sass/");
 
-  eleventyConfig.addPassthroughCopy("./source/img");
-  eleventyConfig.addPassthroughCopy("./source/js");
-  eleventyConfig.addPassthroughCopy("./source/static");
-  eleventyConfig.addPassthroughCopy("./source/browserconfig.xml");
-  eleventyConfig.addPassthroughCopy("./source/CNAME");
-  eleventyConfig.addPassthroughCopy("./source/crossdomain.xml");
-  eleventyConfig.addPassthroughCopy("./source/humans.txt");
-  eleventyConfig.addPassthroughCopy("./source/favicon.ico");
-  eleventyConfig.addPassthroughCopy("./source/favicon-16x16.png");
-  eleventyConfig.addPassthroughCopy("./source/favicon-32x32.png");
-  eleventyConfig.addPassthroughCopy("./source/favicon-48x48.png");
-  eleventyConfig.addPassthroughCopy("./source/favicon.svg");
-  eleventyConfig.addPassthroughCopy("./source/manifest.json");
-  eleventyConfig.addPassthroughCopy("./source/robots.txt");
+  // Filters
+  // Universal slug filter strips unsafe chars from URLs
+  eleventyConfig.addFilter("slugify", function (str) {
+    return slugify(str, {
+      lower: true,
+      replacement: "-",
+      remove: /[*+~.·,()'"`´%!?¿:@»]/g
+    });
+  });
+
+  // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+  eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
+  });
+
+  // Get the first `n` elements of a collection.
+  eleventyConfig.addFilter("head", (array, n) => {
+    if( n < 0 ) {
+      return array.slice(n);
+    }
+
+    return array.slice(0, n);
+  });
+
+  eleventyConfig.addFilter("min", (...numbers) => {
+    return Math.min.apply(null, numbers);
+  });
 
 
-  /* Markdown Overrides */
+  // Misc
+  eleventyConfig.setDataDeepMerge(true);
+
+
+  // Markdown overrides
   let markdownLibrary = markdownIt({
     html: true,
     breaks: true,
@@ -126,16 +141,8 @@ module.exports = function(eleventyConfig) {
       "liquid"
     ],
 
-    // If your site lives in a different subdirectory, change this.
-    // Leading or trailing slashes are all normalized away, so don’t worry about those.
 
-    // If you don’t have a subdirectory, use "" or "/" (they do the same thing)
-    // This is only used for link URLs (it does not affect your file structure)
-    // Best paired with the `url` filter: https://www.11ty.dev/docs/filters/url/
-
-    // You can also pass this in on the command line using `--pathprefix`
-    pathPrefix: "/",
-
+    // Defaults
     markdownTemplateEngine: "liquid",
     htmlTemplateEngine: "njk",
     dataTemplateEngine: "njk",
@@ -143,8 +150,6 @@ module.exports = function(eleventyConfig) {
     // These are all optional, defaults are shown:
     dir: {
       input: "source",
-      // includes: "_includes",
-      // data: "_data",
       output: "published"
     }
   };
